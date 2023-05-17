484718ec0e40595c70454455660883d8a8d5525a
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-05-17 13:20:30 +0900

    [level 2] Title: 가장 큰 수, Time: 1.55 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(int[] numbers) {
+    
+        String answer = "";
+        String []res =new String[numbers.length];
+        for(int i=0; i<numbers.length; i++){
+            res[i]=String.valueOf(numbers[i]);
+          
+        }
+        Arrays.sort(res, (a,b)->{
+            return (a+b).compareTo(b+a);
+        }
+        );
+        
+       //정렬한 값 더하기
+       
+        for(int i=numbers.length-1; i>=0; i--){
+            answer+=res[i];
+        }
+        if(answer.charAt(0)=='0') return "0";
+        
+        return answer;
+    }
+}
