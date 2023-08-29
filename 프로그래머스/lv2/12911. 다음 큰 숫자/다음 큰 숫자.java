c47f65344bf91514cd77fef2934a00f9f75f29b7
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-08-29 19:06:46 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = Integer.bitCount(n);
+        
+        while(answer != Integer.bitCount(++n));
+        
+        return n;
+    }
+}
