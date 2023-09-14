b938949b53f6ee62164cb062768e3f59ee83c615
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-09-14 21:03:03 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 71 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+class Solution {
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> hash = new HashMap<>();
+
+        for(int i=0; i<clothes.length; i++){
+            String cloth_type = clothes[i][1];
+            hash.put(cloth_type, hash.getOrDefault(cloth_type, 1) + 1);
+        }
+
+        for (String key : hash.keySet()) {
+            answer *= hash.get(key);
+        }
+
+        return answer-1;
+    }
+}
