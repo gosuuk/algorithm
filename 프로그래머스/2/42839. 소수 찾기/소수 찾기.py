db934f6d8bae72a70bccf6bbdc4f91ffa020c382
Park SeongWook <116128609+gosuuk@users.noreply.github.com>
2023-11-27 22:03:51 +0900

    [level 2] Title: 소수 찾기, Time: 0.62 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,28 @@
+from itertools import permutations
+import math
+
+def isDecimal(num):
+    if(num <= 1):
+        return False 
+    for i in range(2, num):
+        if num%i == 0:
+            return False    
+    return True
+    
+def solution(numbers):
+    
+    li = list(numbers)
+    allNums = set()
+    for i in range(1, len(numbers)+1):
+        permutationList = permutations(li, i)
+        for perm in permutationList:
+            num = int(''.join(perm))
+            allNums.add(num)
+
+    
+    count = 0
+    for num in allNums:
+        if isDecimal(num):
+            count+=1
+            
+    return count
